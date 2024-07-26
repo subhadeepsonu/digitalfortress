749ef6b62bfec794c49fb24b652d7f792c0900d0
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-07-27 00:19:18 +0530

    moving to retro style

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -17,6 +17,7 @@ const Font = Lato({
     subsets:["latin"],
     weight:["900","400"]
 })
+
 export default function HomePage(){
     const ref1 = useRef(null)
     const ref2 = useRef(null)
@@ -78,40 +79,48 @@ export default function HomePage(){
         
     },[])
     return<div className="h-screen flex-grow w-full bg-white snap-y snap-mandatory scroll-smooth overflow-y-scroll ">
-        <div className="h-16 w-full  z-10 bg-orange-400/10 shadow-sm fixed top-0 backdrop-blur-sm flex justify-between items-center px-5">
+        <div className="h-16 w-full  z-10 bg-orange-400 border-b-[3px]  border-black shadow-sm fixed top-0 backdrop-blur-sm flex justify-between items-center px-5">
         <div className="h-full flex justify-center  items-center">
-        <img ref={ref1} src="https://digitalfortress.in/_next/static/media/logo.7fbc3c85.svg " className="h-2/3"></img>
+        <p ref={ref1} className="text-3xl font-bold  tracking-tighter">DIGITAL FORTRESS</p>
         </div>
-        <div className="w-1/2 flex justify-between items-center font-medium ">
-            <Link className="hover:underline  hover:text-orange-500 " ref={ref2} href={"/#home"} >
-                Home
+        <div className="w-1/2 flex justify-between items-center font-medium  ">
+            <Link className=" text-sm" ref={ref2} href={"/#home"} >
+                HOME
             </Link>
-            <Link className="hover:underline hover:text-orange-500 " ref={ref3} href={"/#products"}>
-                Products
+            <Link className=" text-sm" ref={ref3} href={"/#products"}>
+                PRODUCTS
             </Link>
-            <Link className="hover:underline hover:text-orange-500 " ref={ref4} href={"/#services"}>
-                Services
+            <Link className="  text-sm" ref={ref4} href={"/#services"}>
+                SERVICES
             </Link>
-            <Link className="hover:underline hover:text-orange-500 " ref={ref5} href={"/#testimonial"}>
-                Testimonials
+            <Link className="  text-sm" ref={ref5} href={"/#testimonial"}>
+                TESTIMONIALS
             </Link>
-            <Link className="hover:underline hover:text-orange-500 " ref={ref6} href={"/#aboutus"}>
-                About 
+            <Link className="  text-sm" ref={ref6} href={"/#aboutus"}>
+                ABOUT
             </Link>
-            <Link className="hover:underline hover:text-orange-500 " ref={ref11} href={"/#contactus"}>
-                Contact 
+            <Link className="  text-sm" ref={ref11} href={"/#contactus"}>
+                CONTACT
             </Link>
         </div>
         </div>
-        <div id="home"  className="h-screen w-full  flex justify-around items-center pt-20 snap-start snap-always bg-orange-50 ">
-            <div className="w-3/5 h-2/3 flex flex-col justify-around items-start pl-20">
-                <div ref={ref7} className={`${Font.className} w-full font-extrabold text-7xl flex justify-center items-end pb-2 `}>
-                Protect Your Digital World with Passwordless Authentication.
+        <div id="home"  className="h-screen w-full  flex justify-around items-center pt-16 snap-start snap-always bg-orange-50 ">
+            <div className="w-1/2 h-full flex flex-col justify-center items-start pl-20 border-r-[3px] border-black">
+                <div ref={ref7} className={`${Font.className} flex-col pb-10 tracking-tighter leading-tight w-full font-extrabold text-7xl flex justify-center   `}>
+                    <p>Protect Your Digital World with </p>
+                    <p className="bg-black w-fit px-1 tracking-normal">
+        
+                    <p className="bg-orange-400 -translate-y-2 -translate-x-2  px-1">Passwordless</p>
+                    
+                        </p>
+                    <p> Authentication.</p>
                 </div>
-                <p ref={ref8} className={` text-xl text-orange-500 ${Font.className}`}>DigitalFortress Private Limited offers a secure and convenient passwordless authentication solution to safeguard your online accounts and prevent cyber attacks. Say goodbye to the hassle of remembering passwords and protect your digital identity with ease.</p>
-                <button ref={ref10} className="flex justify-center items-center px-3 py-1 bg-orange-500 text-white rounded-lg text-xl">Contact us!</button>
+                {/* <p ref={ref8} className={` text-xl text-orange-500 ${Font.className} tracking-tighter`}>DigitalFortress Private Limited offers a secure and convenient passwordless authentication solution to safeguard your online accounts and prevent cyber attacks. Say goodbye to the hassle of remembering passwords and protect your digital identity with ease.</p> */}
+                <div ref={ref10} className="bg-black">
+                <button ref={ref10} className="flex z-10  justify-center items-center -translate-y-2 -translate-x-2 px-3 py-1 bg-orange-500 text-white  text-xl">Contact us!</button>
+                </div>
             </div>
-            <div ref={ref9} className=" h-full w-2/5 flex justify-center items-center">
+            <div ref={ref9} className=" h-full w-1/2 flex justify-center items-center">
             <Lottie animationData={homeface}></Lottie>
             </div>
         </div>
@@ -155,7 +164,15 @@ export default function HomePage(){
         </div>
         <div id="contactus" className="h-screen w-full flex flex-col justify-center items-center snap-start snap-always bg-orange-200 ">
             <Title name="CONTACT US"></Title>
-            <div className="w-2/3 shadow-sm h-3/5 flex bg-orange-50 rounded-lg">
+            <motion.div initial={{
+                opacity:0
+            }} whileInView={{
+                opacity:1
+            }}
+            transition={{
+                delay:0.5
+            }}
+            className="w-2/3 shadow-sm h-3/5 flex bg-orange-50 rounded-lg">
             <div className="w-1/2 h-full flex justify-around items-center  flex-col">
                 <div className="w-full h-1/3  flex justify-around items-center flex-col">
                 <p className="text-2xl font-bold">Want to meet in-person?</p>
@@ -165,7 +182,7 @@ export default function HomePage(){
                 </div>
                 <div className="w-full  h-1/3  flex flex-col justify-around items-center">
                     <p className="text-2xl font-bold">Social Links</p>
-                    <div className="w-1/2 justify-between items-center flex"><IoLogoLinkedin className="text-4xl cursor-pointer" /><FaInstagramSquare className="text-4xl cursor-pointer" /><FaTwitter className="text-4xl cursor-pointer" /><FaFacebook className="text-4xl cursor-pointer" /></div>
+                    <div className="w-1/2 justify-between items-center flex"><IoLogoLinkedin className="hover:scale-105 hover:text-orange-800 duration-100 text-5xl cursor-pointer" /><FaInstagramSquare className="hover:scale-105 hover:text-orange-800 duration-100 text-4xl cursor-pointer" /><FaTwitter className="hover:scale-105 hover:text-orange-800 duration-100 text-4xl cursor-pointer" /><FaFacebook className="hover:scale-105 hover:text-orange-800 duration-100 text-4xl cursor-pointer" /></div>
                 </div>
             </div>
             <div className="">
@@ -176,7 +193,7 @@ export default function HomePage(){
                 <p className="font-bold text-2xl">Mail Us</p>
                 <div className="flex flex-col"> 
                 <label>email</label>
-                <input className="rounded-lg w-full p-2 mt-2" type="email"></input>
+                <input className=" rounded-lg w-full p-2 mt-2" type="email"></input>
                 </div>
                 <div>
                 <label>content</label>
@@ -187,7 +204,7 @@ export default function HomePage(){
                 </div>
                 
         
-            </div>
+            </motion.div>
         </div>
     </div>
 }
